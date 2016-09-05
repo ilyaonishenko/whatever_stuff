df673507ff54f7f0eeaf345452ffeb9bbac70af4
wopqw <wopqw@yandex.ru>
2016-09-05 19:43:57 +0300

    adding gradle dependencies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
+import java.util.function.BinaryOperator;
+
 /**
  * Created by wopqw on 03.09.16.
  */
 public class Main {
 
+    public static void main(String[] args) {
+
+
+        BinaryOperator<Integer> addInt = (x, y) -> x + y;
+
+        System.out.println(addInt.apply(5,5));
+    }
 }
