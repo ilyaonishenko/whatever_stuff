ca5a2607690842764c850fff325eaf60d0e9d876
wopqw <wopqw@yandex.ru>
2016-09-06 03:00:45 +0300

    update gradle and gitignore

---
+++ src/main/java/MonteCarlo.java
@@ -0,0 +1,37 @@
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Created by wopqw on 06.09.16.
+ */
+class MonteCarlo {
+
+    static Map<Integer,Double> parallelDiceRolls(int N){
+
+        double fraction = 1.0/N;
+
+        return IntStream.range(0,N)
+                .parallel()
+                .mapToObj(twoDiceThrows())
+                .collect(Collectors.groupingBy(side -> (Integer)side, Collectors.summingDouble(n -> fraction)));
+    }
+
+    private static IntFunction<Integer> twoDiceThrows(){
+
+        int firstThrow = ThreadLocalRandom.current().nextInt(1,7);
+        int secondThrow = ThreadLocalRandom.current().nextInt(1,7);
+
+        Integer answer = firstThrow+secondThrow;
+
+        return value -> answer;
+//        return (firstThrow,secondThrow) -> firstThrow+secondThrow;
+    }
+
+
+
+
+
+}
---
+++ src/test/java/MonteCarloTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by wopqw on 06.09.16.
+ */
+public class MonteCarloTest {
+    @Test
+    public void parallelDiceRolls() throws Exception {
+
+//        Assert.assertThat();
+        System.out.println(MonteCarlo.parallelDiceRolls(10));
+
+    }
+
+}
