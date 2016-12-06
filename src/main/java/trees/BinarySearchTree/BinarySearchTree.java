4da7e80b7bf3bee2f3acab30da9466fe5f928cc5
wopqw <wopqw@yandex.ru>
2016-12-06 09:53:26 +0300

    traverse binary search tree

--- src/main/java/trees/BinarySearchTree/BinarySearchTree.java
+++ src/main/java/trees/BinarySearchTree/BinarySearchTree.java
@@ -2,38 +2,41 @@ package trees.BinarySearchTree;
 
 import trees.Node;
 
+import java.util.Stack;
+
 /**
  * Created by wopqw on 05.12.16.
  */
+@SuppressWarnings("Duplicates")
 public class BinarySearchTree {
 
     private Node root;
 
-    public BinarySearchTree(){
+    public BinarySearchTree() {
         root = null;
     }
 
-    public Node find(int key){
+    public Node find(int key) {
 
         Node current = root;
 
-        while(current.key != key){
+        while (current.key != key) {
 
             if (current.key < key)
                 current = root.leftChild;
             else current = root.rightChild;
 
-            if(current == null)
+            if (current == null)
                 return null;
         }
         return current;
     }
 
-    public void insert(int key, double data){
+    public void insert(int key, double data) {
 
         Node newNode = new Node(key, data);
 
-        if(root == null) {
+        if (root == null) {
             root = newNode;
             return;
         }
@@ -41,19 +44,19 @@ public class BinarySearchTree {
         Node current = root;
         Node parent;
 
-        while(true){
+        while (true) {
 
             parent = current;
 
-            if(current.key < newNode.key) {
+            if (current.key < newNode.key) {
                 current = current.leftChild;
-                if(current == null) {
+                if (current == null) {
                     parent.leftChild = newNode;
                     return;
                 }
             } else {
                 current = current.rightChild;
-                if(current == null) {
+                if (current == null) {
                     parent.rightChild = newNode;
                     return;
                 }
@@ -62,15 +65,15 @@ public class BinarySearchTree {
         }
     }
 
-    public boolean delete(int key){
+    public boolean delete(int key) {
 
         Node current = root;
         Node parent = root;
         boolean isLeftChild = true;
 
-        while(current.key != key){
+        while (current.key != key) {
             parent = current;
-            if(current.key < key) {
+            if (current.key < key) {
                 isLeftChild = true;
                 current = current.leftChild;
             } else {
@@ -80,21 +83,21 @@ public class BinarySearchTree {
             if (current == null)
                 return false;
         }
-        if(current.leftChild == null && current.rightChild == null){
+        if (current.leftChild == null && current.rightChild == null) {
             if (current == root)
                 root = null;
             else if (isLeftChild)
                 parent.leftChild = null;
             else
                 parent.rightChild = null;
-        } else if (current.rightChild == null){
+        } else if (current.rightChild == null) {
             if (current == root)
                 root = current.leftChild;
             else if (isLeftChild)
                 parent.leftChild = current.leftChild;
             else
                 parent.rightChild = current.leftChild;
-        } else if (current.leftChild == null){
+        } else if (current.leftChild == null) {
             if (current == root)
                 root = current.rightChild;
             else if (isLeftChild)
@@ -104,7 +107,7 @@ public class BinarySearchTree {
         } else {
             Node successor = getSuccessor(current);
 
-            if(current == root)
+            if (current == root)
                 root = successor;
             else if (isLeftChild)
                 parent.leftChild = successor;
@@ -114,24 +117,105 @@ public class BinarySearchTree {
         return true;
     }
 
-    private Node getSuccessor(Node delNode){
+    private Node getSuccessor(Node delNode) {
 
         Node successorParent = delNode;
         Node successor = delNode;
         Node current = delNode.rightChild;
 
-        while (current != null){
+        while (current != null) {
 
             successorParent = successor;
             successor = current;
             current = current.leftChild;
         }
 
-        if(successor != delNode.rightChild){
+        if (successor != delNode.rightChild) {
             successor.leftChild = successor.rightChild;
             successor.rightChild = delNode.rightChild;
         }
 
         return successor;
+    }
+
+    public void traverse(int traverseType) {
+
+        switch (traverseType) {
+            case 1:
+                System.out.println("PreOrder");
+                preOrder(root);
+                break;
+            case 2:
+                System.out.println("inOrder");
+                inOrder(root);
+                break;
+            case 3:
+                System.out.println("postOrder");
+                postOrder(root);
+                break;
+        }
+    }
+
+    private void preOrder(Node localRoot) {
+
+        if (localRoot != null) {
+            System.out.println(localRoot.data);
+            preOrder(localRoot.leftChild);
+            preOrder(localRoot.rightChild);
+        }
+    }
+
+    private void inOrder(Node localRoot) {
+
+        if (localRoot != null) {
+            inOrder(localRoot.leftChild);
+            System.out.println(localRoot.data);
+            inOrder(localRoot.rightChild);
+        }
+    }
+
+    private void postOrder(Node localRoot) {
+
+        if (localRoot != null) {
+            postOrder(localRoot.leftChild);
+            postOrder(localRoot.rightChild);
+            System.out.println(localRoot.data);
+        }
+    }
+
+    public void displayTree() {
+        Stack<Node> globalStack = new Stack<>();
+        globalStack.push(root);
+        int nBlanks = 32;
+        boolean isRowEmpty = false;
+        System.out.println("......................................................");
+        while (!isRowEmpty) {
+            Stack<Node> localStack = new Stack<>();
+            isRowEmpty = true;
+            for (int j = 0; j < nBlanks; j++)
+                System.out.print(' ');
+            while (!globalStack.isEmpty()) {
+                Node temp = globalStack.pop();
+                if (temp != null) {
+                    System.out.print(temp.data);
+                    localStack.push(temp.leftChild);
+                    localStack.push(temp.rightChild);
+                    if (temp.leftChild != null ||
+                            temp.rightChild != null)
+                        isRowEmpty = false;
+                } else {
+                    System.out.print("--");
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+                for (int j = 0; j < nBlanks * 2 - 2; j++)
+                    System.out.print(' ');
+            }
+            System.out.println();
+            nBlanks /= 2;
+            while (!localStack.isEmpty())
+                globalStack.push(localStack.pop());
+        }
+        System.out.println("......................................................");
     }
 }
